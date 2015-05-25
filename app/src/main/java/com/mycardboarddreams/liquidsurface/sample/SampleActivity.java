647c389848661dd53ed414234031fd638d1f048d
abc <a@b.c>
2015-05-24 21:38:41 -0400

    Pause and resume particle view.

--- app/src/main/java/com/mycardboarddreams/liquidsurface/sample/SampleActivity.java
+++ app/src/main/java/com/mycardboarddreams/liquidsurface/sample/SampleActivity.java
@@ -21,16 +21,27 @@ public class SampleActivity extends ActionBarActivity {
         }
     }
 
+    LiquidTextureView ltv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sample);
 
-        LiquidTextureView ltv = (LiquidTextureView) findViewById(R.id.liquid_texture_view);
+        ltv = (LiquidTextureView) findViewById(R.id.liquid_texture_view);
 
         ltv.createLiquidShape(new float[]{100, 100, 200, 200, 100, 300}, 0xFF00FF00, 1);
-
-        ltv.resumeParticles();
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        ltv.pauseParticles();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ltv.resumeParticles();
+    }
 }
