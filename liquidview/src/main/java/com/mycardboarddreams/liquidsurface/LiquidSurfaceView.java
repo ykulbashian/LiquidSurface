e1a1af7b779154a1f50133125b54ec3f4ddbb5b3
ABC <abc@ABCs-MacBook-Pro.local>
2015-09-19 17:08:42 -0400

    Added GLSurfaceView implementation (faster).

---
+++ liquidview/src/main/java/com/mycardboarddreams/liquidsurface/LiquidSurfaceView.java
@@ -0,0 +1,155 @@
+package com.mycardboarddreams.liquidsurface;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+
+import com.google.fpl.liquidfunpaint.GroupOptions;
+import com.google.fpl.liquidfunpaint.ILiquidWorld;
+import com.google.fpl.liquidfunpaint.LiquidWorld;
+import com.google.fpl.liquidfunpaint.ParticleSystems;
+import com.google.fpl.liquidfunpaint.SolidWorld;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by abc on 15-09-19.
+ */
+public class LiquidSurfaceView extends GLSurfaceView implements ILiquidWorld, GLSurfaceView.Renderer {
+
+    /**
+     * Load the native libraries
+     */
+    static {
+        try{
+            System.loadLibrary("liquidfun");
+            System.loadLibrary("liquidfun_jni");
+
+        } catch (UnsatisfiedLinkError e) {
+            // only ignore exception in non-android env. This is to aid Robolectric integration.
+            if ("Dalvik".equals(System.getProperty("java.vm.name"))) throw e;
+        }
+    }
+
+    private RotatableController mController;
+
+    final private Queue<Runnable> pendingRunnables = new ConcurrentLinkedQueue<>();
+
+    public LiquidSurfaceView(Context context) {
+        super(context);
+        initialize(context);
+    }
+
+    public LiquidSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initialize(context);
+    }
+
+    protected void initialize(Context context) {
+
+        if(isInEditMode())
+            return;
+
+        com.google.fpl.liquidfunpaint.renderer.Renderer.getInstance().init((Activity) context);
+
+        setEGLContextClientVersion(2);
+        setEGLConfigChooser(8, 8, 8, 8, 16, 0);
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        if (BuildConfig.DEBUG) {
+            setDebugFlags(
+                    GLSurfaceView.DEBUG_LOG_GL_CALLS |
+                            GLSurfaceView.DEBUG_CHECK_GL_ERROR);
+        }
+
+        setRenderer(this);
+
+        mController = new RotatableController((Activity)context);
+    }
+
+    @Override
+    public void resumePhysics() {
+        mController.updateDownDirection((Activity) getContext());
+        com.google.fpl.liquidfunpaint.renderer.Renderer.getInstance().startSimulation();
+        mController.onResume();
+    }
+
+    @Override
+    public void pausePhysics(){
+        com.google.fpl.liquidfunpaint.renderer.Renderer.getInstance().pauseSimulation();
+        mController.onPause();
+    }
+
+    @Override
+    public void createLiquidShape(final float[] vertices){
+
+        pendingRunnables.add(new Runnable() {
+            @Override
+            public void run() {
+                ParticleSystems.getInstance().fillShape(normalizePositions(vertices), GroupOptions.LIQUID, ParticleSystems.DEFAULT_PARTICLE_SYSTEM);
+            }
+        });
+    }
+
+    @Override
+    public void createSolidShape(final float[] vertices){
+        pendingRunnables.add(new Runnable() {
+            @Override
+            public void run() {
+                SolidWorld.getInstance().createSolidObject(normalizePositions(vertices));
+            }
+        });
+    }
+
+    @Override
+    public void emptyShape(final float[] vertices){
+        pendingRunnables.add(new Runnable() {
+            @Override
+            public void run() {
+                ParticleSystems.getInstance().emptyShape(normalizePositions(vertices));
+            }
+        });
+    }
+
+    private float getWidthRatio(){
+        return LiquidWorld.getInstance().sRenderWorldWidth / getWidth();
+    }
+
+    private float getHeightRatio(){
+        return LiquidWorld.getInstance().sRenderWorldHeight / getHeight();
+    }
+
+    private float[] normalizePositions(float[] originalVertices){
+        float[] normalizedVerts = new float[originalVertices.length];
+
+        for(int i = 0; i < originalVertices.length; i++){
+            normalizedVerts[i] = originalVertices[i] * ((i % 2 == 0) ? getWidthRatio() : getHeightRatio());
+        }
+
+        return normalizedVerts;
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        com.google.fpl.liquidfunpaint.renderer.Renderer.getInstance().onSurfaceCreated(gl, config);
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        com.google.fpl.liquidfunpaint.renderer.Renderer.getInstance().onSurfaceChanged(gl, width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+
+        while (!pendingRunnables.isEmpty()) {
+            pendingRunnables.poll().run();
+        }
+        com.google.fpl.liquidfunpaint.renderer.Renderer.getInstance().onDrawFrame(gl);
+    }
+}
