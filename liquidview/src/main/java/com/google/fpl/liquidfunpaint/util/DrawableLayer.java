00a162d5185cfbf263c0bc264eed6c20fca569fc
ABC <abc@ABCs-MacBook-Pro.local>
2015-09-20 20:06:02 -0400

    Removed unnecessary GLTextureView file.

--- liquidview/src/main/java/com/google/fpl/liquidfunpaint/util/DrawableLayer.java
+++ liquidview/src/main/java/com/google/fpl/liquidfunpaint/util/DrawableLayer.java
@@ -2,12 +2,16 @@ package com.google.fpl.liquidfunpaint.util;
 
 import android.content.Context;
 
-import com.google.fpl.liquidfunpaint.GLTextureView;
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
 
 /**
  * Created on 8/16/2015.
  */
-public interface DrawableLayer extends GLTextureView.Renderer{
+public interface DrawableLayer {
     void reset();
     void init(Context activity);
+    void onSurfaceCreated(GL10 gl, EGLConfig config);
+    void onSurfaceChanged(GL10 gl, int width, int height);
+    void onDrawFrame(GL10 gl);
 }
